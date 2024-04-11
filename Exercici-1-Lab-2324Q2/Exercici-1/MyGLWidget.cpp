04dd2da387734e128f399a6acb2430be2f808723
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2024-04-11 08:28:48 +0200

    Add files via upload

--- Exercici-1-Lab-2324Q2/Exercici-1/MyGLWidget.cpp
+++ Exercici-1-Lab-2324Q2/Exercici-1/MyGLWidget.cpp
@@ -20,86 +20,74 @@ void MyGLWidget::initializeGL ()
   
   glClearColor (0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
   carregaShaders();
-  creaBuffersRectangle();
-  creaBuffersCano();
-  creaBuffersRoda();
+  creaBuffersQuadrat();
 }
 
 
 void MyGLWidget::modelTransformQuadrat(glm::vec3 posicio, glm::vec3 escala)
 {
   glm::mat4 TG(1.0f);
-  TG = glm::translate(TG,posicio);  
+  TG = glm::translate(TG,posicio);
   TG = glm::scale(TG,escala);
-  glUniformMatrix4fv(TGLoc, 1, GL_FALSE, &TG[0][0]);
-}
-
-void MyGLWidget::modelTransformQuadratCano()
-{
-  glm::mat4 TG(1.0f);
-  TG = glm::translate(TG,glm::vec3(tx + 0.125,0.25,0.0));
-  TG = glm::rotate(TG, rotacio, glm::vec3(0.0,0.0,1.0));
-  TG = glm::translate(TG,glm::vec3(-0.125,-0.25,0.0));
-  
-  glUniformMatrix4fv(TGLoc, 1, GL_FALSE, &TG[0][0]);
-}
-
-void MyGLWidget::modelTransformQuadratRoda(float shift, float angle) 
-{
-  glm::mat4 TG(1.0f);
-  TG = glm::translate(TG,glm::vec3(tx - 0.375 + shift,-0.125,0.0));
-  TG = glm::rotate(TG, angle + rotacio2, glm::vec3(0.0,0.0,1.0));
-  TG = glm::translate(TG,glm::vec3(0.375,0.125,0.0));
-  
+  TG = glm::translate(TG,glm::vec3(0.5,0.5,0.0));  
   glUniformMatrix4fv(TGLoc, 1, GL_FALSE, &TG[0][0]);
 }
 
 void MyGLWidget::pintaCos() {
+  glBindVertexArray(VAOQuadrat);
+  modelTransformQuadrat(glm::vec3(tx,0.0,0.0), glm::vec3(1.0,0.25,1.0));
+  glUniform3fv(colorLoc, 1, &verd[0]);
+  glDrawArrays(GL_TRIANGLES, 0, 6);    		
 
-  glBindVertexArray(VAORec);
-  modelTransformQuadrat(glm::vec3(tx,0.0,0.0), glm::vec3(1.0));      
-  glDrawArrays(GL_TRIANGLES, 0, 6);    		                    
-  // Desactivem el VAO
-  glBindVertexArray(0); 
+  modelTransformQuadrat(glm::vec3(tx + 0.125,0.25,0.0), glm::vec3(0.5,0.25,1.0));
+  glUniform3fv(colorLoc, 1, &verd[0]);
+  glDrawArrays(GL_TRIANGLES, 0, 6);    		
+}
 
-  glBindVertexArray(VAORec);    
-  modelTransformQuadrat(glm::vec3(0.125 + tx,0.25,0.0), glm::vec3(0.5,1.0,1.0));      
-  glDrawArrays(GL_TRIANGLES, 0, 6);    		                    
-  // Desactivem el VAO
-  glBindVertexArray(0); 
+void MyGLWidget::modelTransformQuadratCano(glm::vec3 escala) {
+  glm::mat4 TG(1.0f);
+
+  TG = glm::translate(TG,glm::vec3(0.125,0.25,0.0));
+  TG = glm::rotate(TG, rotacio, glm::vec3(0.0,0.0,1.0));
+  TG = glm::translate(TG,glm::vec3(tx + 0.375,0.0,0.0));
+  TG = glm::scale(TG,escala);
+  TG = glm::translate(TG,glm::vec3(0.5,0.5,0.0));  
+  glUniformMatrix4fv(TGLoc, 1, GL_FALSE, &TG[0][0]);
 }
 
 void MyGLWidget::pintaCano() {
-  glBindVertexArray(VAOCano);
-  modelTransformQuadratCano();      
-  glDrawArrays(GL_TRIANGLES, 0, 6);    		                    
-  // Desactivem el VAO
-  glBindVertexArray(0);
+  modelTransformQuadratCano(glm::vec3(0.75,0.125,1.0));
+  glUniform3fv(colorLoc, 1, &gris[0]);
+  glDrawArrays(GL_TRIANGLES, 0, 6);
 }
 
-void MyGLWidget::pintaRodes() {
+void MyGLWidget::modelTransformQuadratRoda(glm::vec3 escala, float shift, float angle) {
+  glm::mat4 TG(1.0f);
+  TG = glm::translate(TG,glm::vec3(shift + tx -0.375,-0.125,0.0));
+  TG = glm::rotate(TG, rotacio2 + angle, glm::vec3(0.0,0.0,1.0)); 
+  TG = glm::translate(TG,glm::vec3(0.0,0.1,0.0));
+  TG = glm::scale(TG,escala);
+  TG = glm::translate(TG,glm::vec3(0.5,0.5,0.0));  
+  glUniformMatrix4fv(TGLoc, 1, GL_FALSE, &TG[0][0]);
+}
 
+void MyGLWidget::pintaRoda() {
   for (int k = 0; k < 4; ++k) {
-    for (int i = 0; i < 6; ++i) {
-      glBindVertexArray(VAOGW);
-      modelTransformQuadratRoda(shift_x[k], vecRot[i]);      
-      glDrawArrays(GL_TRIANGLES, 0, 6);    		                    
-      // Desactivem el VAO
-      glBindVertexArray(0);
-
-      glBindVertexArray(VAOBW);
-      modelTransformQuadratRoda(shift_x[k], vecRot[i]);      
-      glDrawArrays(GL_TRIANGLES, 0, 6);    		                    
-      // Desactivem el VAO
-      glBindVertexArray(0);
+    for (int i = 0; i < 12; ++i) {
+      modelTransformQuadratRoda(glm::vec3(0.05,0.1,1.0), shift_x[k], vecRot[i]);     
+      if (i%2 == 0) glUniform3fv(colorLoc, 1, &gris[0]);
+      else glUniform3fv(colorLoc, 1, &negre[0]);
+      glDrawArrays(GL_TRIANGLES, 0, 6);    		                      		                    
     }
   }
 }
 
+
+
 void MyGLWidget::pintaTanc() {
   pintaCos();
   pintaCano();
-  pintaRodes();
+  pintaRoda();
 }
 
 void MyGLWidget::paintGL ()
@@ -110,8 +98,11 @@ void MyGLWidget::paintGL ()
   
   glClear (GL_COLOR_BUFFER_BIT);  // Esborrem el frame-buffer
 
-  // Pintem un tanc
-  pintaTanc();                           
+  // Pintem un quadrat
+  pintaTanc();
+
+  // Desactivem el VAO
+  glBindVertexArray(0);
 }
 
 void MyGLWidget::resizeGL (int w, int h)
@@ -132,11 +123,11 @@ void MyGLWidget::keyPressEvent(QKeyEvent* event)
 {
   makeCurrent();
   switch (event->key()) {
-    case Qt::Key_Left:
-      if (rotacio < M_PI) rotacio += M_PI/18; 
+    case Qt::Key_Left: 
+      if (rotacio < M_PI - 0.01) rotacio += M_PI/18; 
     	break;
     case Qt::Key_Right: 
-      if (rotacio > 0) rotacio -= M_PI/18;   
+      if (rotacio > 0) rotacio -= M_PI/18;  
     	break;
     case Qt::Key_A: 
       tx -= 0.01;
@@ -151,149 +142,37 @@ void MyGLWidget::keyPressEvent(QKeyEvent* event)
   update();
 }
 
-void MyGLWidget::creaBuffersRectangle()
+void MyGLWidget::creaBuffersQuadrat ()
 {
-  glm::vec3 VertRec1[6];  // vèrtexs amb X, Y i Z
+  glm::vec3 Vertices[6];  // vèrtexs amb X, Y i Z
   
   // minX = -1.0
   // maxX = 0.0
   // minY = -1.0
   // maxY = 0.0
   
-  VertRec1[0] = glm::vec3(-0.5, 0.125, 0);
-  VertRec1[1] = glm::vec3( 0.5, -0.125, 0); 
-  VertRec1[2] = glm::vec3( 0.5, 0.125, 0);
-  VertRec1[3] = glm::vec3( 0.5, -0.125, 0);
-  VertRec1[4] = glm::vec3( -0.5, 0.125, 0);
-  VertRec1[5] = glm::vec3( -0.5, -0.125, 0);
+  Vertices[0] = glm::vec3(-1.0, 0.0, 0);
+  Vertices[1] = glm::vec3( 0.0, -1.0, 0);
+  Vertices[2] = glm::vec3( 0.0, 0.0, 0);
+  Vertices[3] = glm::vec3( 0.0, -1.0, 0);
+  Vertices[4] = glm::vec3( -1.0, 0.0, 0);
+  Vertices[5] = glm::vec3( -1.0, -1.0, 0);
   
   // Creació del Vertex Array Object (VAO) que usarem per pintar el quadrat
-  glGenVertexArrays(1, &VAORec);
-  glBindVertexArray(VAORec);
+  glGenVertexArrays(1, &VAOQuadrat);
+  glBindVertexArray(VAOQuadrat);
 
   // Creació del buffer amb les posicions dels vèrtexs
-  GLuint VBO1;
-  glGenBuffers(1, &VBO1);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertRec1), VertRec1, GL_STATIC_DRAW);
+  GLuint VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
   glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
   glEnableVertexAttribArray(vertexLoc);
-
-  glm::vec3 VertCol1[6];  // vèrtexs amb X, Y i Z
-
-  for (int i = 0; i < 6; ++i) VertCol1[i] = verd;
-
-  GLuint VBO2;
-  glGenBuffers(1, &VBO2);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertCol1), VertCol1, GL_STATIC_DRAW);
-  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(colorLoc);
 
   // Desactivem el VAO
   glBindVertexArray(0);
 }
-
-void MyGLWidget::creaBuffersCano()
-{
-  glm::vec3 VertCano[6];
-  VertCano[0] = glm::vec3(0.125, 0.3125, 0);
-  VertCano[1] = glm::vec3( 0.875, 0.1875, 0); 
-  VertCano[2] = glm::vec3( 0.875, 0.3125, 0);
-  VertCano[3] = glm::vec3( 0.875, 0.1875, 0);
-  VertCano[4] = glm::vec3( 0.125, 0.3125, 0);
-  VertCano[5] = glm::vec3( 0.125, 0.1875, 0);
-
-  glGenVertexArrays(1, &VAOCano);
-  glBindVertexArray(VAOCano);
-
-  // Creació del buffer amb les posicions dels vèrtexs
-  GLuint VBO1;
-  glGenBuffers(1, &VBO1);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertCano), VertCano, GL_STATIC_DRAW);
-  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(vertexLoc);
-
-  glm::vec3 VertCol[6];  // vèrtexs amb X, Y i Z
-
-  for (int i = 0; i < 6; ++i) VertCol[i] = gris;
-
-  GLuint VBO2;
-  glGenBuffers(1, &VBO2);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertCol), VertCol, GL_STATIC_DRAW);
-  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(colorLoc);
-
-  // Desactivem el VAO
-  glBindVertexArray(0);
-
-}
-
-void MyGLWidget::creaBuffersRoda()
-{
-  glm::vec3 VertRoda[6];
-  glm::vec3 VertCol[6];
-  VertRoda[0] = glm::vec3(-0.4, 0.025, 0);
-  VertRoda[1] = glm::vec3( -0.35, -0.075, 0); 
-  VertRoda[2] = glm::vec3( -0.35, 0.025, 0);
-  VertRoda[3] = glm::vec3( -0.35, -0.075, 0);
-  VertRoda[4] = glm::vec3( -0.4, 0.025, 0);
-  VertRoda[5] = glm::vec3( -0.4, -0.075, 0);
-
-  glGenVertexArrays(1, &VAOGW);
-  glBindVertexArray(VAOGW);
-
-  GLuint VBO1;
-  glGenBuffers(1, &VBO1);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertRoda), VertRoda, GL_STATIC_DRAW);
-  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(vertexLoc);
-
-  for (int j = 0; j < 6; ++j) VertCol[j] = gris; 
-
-  GLuint VBO2;
-  glGenBuffers(1, &VBO2);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertCol), VertCol, GL_STATIC_DRAW);
-  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(colorLoc);
-
-  glBindVertexArray(0);
-
-  glm::vec3 VertRoda2[6];
-  glm::vec3 VertCol2[6];
-  VertRoda2[0] = glm::vec3(-0.325, -0.1, 0);
-  VertRoda2[1] = glm::vec3( -0.225, -0.15, 0); 
-  VertRoda2[2] = glm::vec3( -0.225, -0.1, 0);
-  VertRoda2[3] = glm::vec3( -0.225, -0.15, 0);
-  VertRoda2[4] = glm::vec3( -0.325, -0.1, 0);
-  VertRoda2[5] = glm::vec3( -0.325, -0.15, 0);
-
-  glGenVertexArrays(1, &VAOBW);
-  glBindVertexArray(VAOBW);
-
-  GLuint VBO3;
-  glGenBuffers(1, &VBO3);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO3);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertRoda2), VertRoda2, GL_STATIC_DRAW);
-  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(vertexLoc);
-
-  for (int j = 0; j < 6; ++j) VertCol2[j] = negre; 
-
-  GLuint VBO4;
-  glGenBuffers(1, &VBO4);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO4);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(VertCol2), VertCol2, GL_STATIC_DRAW);
-  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  glEnableVertexAttribArray(colorLoc);
-
-  glBindVertexArray(0);
-}
-
 
 
 void MyGLWidget::carregaShaders()
@@ -319,6 +198,6 @@ void MyGLWidget::carregaShaders()
   
   // Obtenim els identificadors dels uniforms
   TGLoc = glGetUniformLocation(program->programId(), "TG"); 
-  rotLoc = glGetUniformLocation(program->programId(), "TG");
-  colorLoc = glGetAttribLocation(program->programId(), "colors");
+  colorLoc = glGetUniformLocation(program->programId(), "colors");
+
 }
--- Exercici-1-Lab-2324Q2/Exercici-1/MyGLWidget.h
+++ Exercici-1-Lab-2324Q2/Exercici-1/MyGLWidget.h
@@ -6,9 +6,9 @@
 #include <QKeyEvent>
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include <vector>
 
-using namespace std;
+#include "glm/gtc/matrix_transform.hpp"
+#define GLM_FORCE_RADIANS
 
 class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
 {
@@ -34,41 +34,38 @@ class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
 
   private:
     
-    void creaBuffersRectangle();
-
-    void creaBuffersCano();
-
-    void creaBuffersRoda();
+    void creaBuffersQuadrat();
     
     void carregaShaders();
     
     void modelTransformQuadrat(glm::vec3 posicio, glm::vec3 escala);
 
-    void modelTransformQuadratCano();
+    void modelTransformQuadratCano(glm::vec3 escala);
 
-    void modelTransformQuadratRoda(float shift, float angle);
+    void modelTransformQuadratRoda(glm::vec3 escala, float shift, float angle);
 
     void pintaTanc();
     void pintaCos();
     void pintaCano();
-    void pintaRodes();
+    void pintaRoda();
     
     // program
     QOpenGLShaderProgram *program;
     // attribute locations
-    GLuint vertexLoc, colorLoc, rotLoc;
+    GLuint vertexLoc, colorLoc;
     // uniform locations
     GLuint TGLoc;
 
     // VAOs
-    GLuint VAORec, VAOCano, VAOGW, VAOBW;
+    GLuint VAOQuadrat;
+
     // viewport
     GLint ample, alt;    
 
-    float rotacio = 0.0, tx = 0.0, rotacio2 = 0.0;
+    float tx = 0.0, rotacio2 = 0.0, rotacio = 0.0;
 
-    float vecRot[6] = {0.0,M_PI/3,2*(M_PI/3),M_PI, 4*M_PI/3, 5*M_PI/3};
     float shift_x[4] = {0.0,0.25, 0.5, 0.75};
+    float vecRot[12] = {0.0,M_PI/6,M_PI/3,M_PI/2, 2*(M_PI/3),5*M_PI/6,M_PI,7*M_PI/6 ,4*M_PI/3, 3*M_PI/2, 5*M_PI/3, 11*M_PI/6};
     
     // colors
     glm::vec3 verd = glm::vec3(0.0,0.6,0.0);
